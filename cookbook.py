80e349c7e59bbceacf22066701189df647414db9
CMorton737 <CMorton737@gmail.com>
2016-12-08 14:04:31 -0500

    Added float rounding

--- cookbook.py
+++ cookbook.py
@@ -5,9 +5,9 @@ game=league.team('CHAD MORTON').week(4)
 
 print('%s vs. %s' % (game.team_name,game.opponent().team_name))
 for plyr,opp_plyr in zip(game.lineup,game.opponent().lineup):
-    m='%s:\t%s\t%s\t\t%s\t%s'
+    m='%s:\t%s\t%.1f\t\t%s\t%.1f'
     print(m % (plyr.slot,plyr.gsis_name,plyr.statistics().score(),opp_plyr.gsis_name,opp_plyr.statistics().score()))
-print('%s %s %s-%s' % (game.team_name,'win' if game.win() else 'lose',game.get_score(),game.opponent().get_score()))
+print('%s %s %.1f-%.1f' % (game.team_name,'win' if game.win() else 'lose',game.get_score(),game.opponent().get_score()))
 
 
                             
