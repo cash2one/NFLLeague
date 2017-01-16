dfb1655d987e0058b84a000e113b45fa1bd22492
Chad Morton <CMorton737@gmail.com>
2017-01-16 17:25:49 -0500

    Added example data

--- playground.py
+++
@@ -1,7 +0,0 @@
-import nflleague
-
-scraper=nflleague.update.Generate(203986,2016,'Firefox')
-scraper.init_league()
-for week in range(1,14):
-    scraper.update_lineups_by_week(week,force=True)
-scraper.close()
